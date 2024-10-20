d991ce98fb388b52db0279e9dfdf3df10a339e4b
fuwari-chan <fuwarichank@gmail.com>
2024-10-20 23:56:51 +0700

    rebuilding site Sun Oct 20 23:56:51 WIB 2024

---
+++ chunks/chunk.DihuBdzv.js
@@ -0,0 +1,1 @@
+async function getMod(){return import("./chunk.aF9IiJ-2.js")}const collectedLinks=[],collectedStyles=[],collectedScripts=[],defaultMod={__astroPropagation:!0,getMod:getMod,collectedLinks:collectedLinks,collectedStyles:collectedStyles,collectedScripts:collectedScripts};export{defaultMod as default};
